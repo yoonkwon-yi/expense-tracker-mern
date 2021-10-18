9582d1d940d0955fdef271d25d99c079a8186d76
yoonkwon-yi <yoonkwon.yi@gmail.com>
2021-10-18 02:46:31 -0500

    add script

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.use('/api/v1/transactions', transactions)
 
 // app.get('/', (req, res) => res.send('Hello'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  )
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'))
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   )
+// }
 
 //access env variable that we set by using dotenv by using syntax "process.env"
 const PORT = process.env.PORT || 5000
