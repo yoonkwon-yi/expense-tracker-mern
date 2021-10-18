cd8f2cfafabac45c3701a079a381046a850aac5a
yoonkwon-yi <yoonkwon.yi@gmail.com>
2021-10-18 02:17:46 -0500

    add build folder

---
+++ client/build/precache-manifest.4a9c74d1de969d203cfc7287b3282be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e39867df9cfcc5f90ec41086838aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a396b36c2d9b3ee0d814",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "7f41ee905f9331c63743",
+    "url": "/static/js/2.1d97a376.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1d97a376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a396b36c2d9b3ee0d814",
+    "url": "/static/js/main.37df4980.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
